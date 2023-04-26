88e8cc7fdb4d97aa226da17eb43431f9643374b1
Floraaaaa99 <130607640+Floraaaaa99@users.noreply.github.com>
2023-04-26 14:31:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+document.querySelector("h1").classList.add("play");
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector(".play").innerHTML= "Player 1 Wins!"
+}
+else if (randomNumber1 < randomNumber2) {
+  document.querySelector(".play").innerHTML= "Player 2 Wins!"
+}
+else if (randomNumber1 = randomNumber2){
+   document.querySelector(".play").innerHTML= "Draw!"
+}
